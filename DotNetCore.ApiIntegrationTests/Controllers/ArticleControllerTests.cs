9489365467ac4cbed23b1e520c3b1d5647b118cc
Jiten P <panchal.jiten.uk@gmail.com>
2018-09-21 12:39:03 +0100

    Renamed tests

--- DotNetCore.ApiIntegrationTests/Controllers/ArticleControllerTests.cs
+++ DotNetCore.ApiIntegrationTests/Controllers/ArticleControllerTests.cs
@@ -4,6 +4,7 @@ using DotNetCore.Models.Request;
 using DotNetCore.Models.Response;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,18 +12,16 @@ using System.Threading.Tasks;
 namespace DotNetCore.ApiIntegrationTests.Controllers
 {
     [TestClass]
-    public class TestControllerTests : BaseApiIntegrationTest
+    public class ArticleControllerTests : BaseApiIntegrationTest
     {
         [TestInitialize]
         public void OnTestInitialize()
         {
-           
         }
 
         [TestCleanup]
         public void OnTestCleanup()
         {
-           
         }
 
         #region Get
@@ -53,7 +52,7 @@ namespace DotNetCore.ApiIntegrationTests.Controllers
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
 
-            var getResponse = JsonConvert.DeserializeObject<TestPagedList<ArticleResponse>>(responseString); 
+            var getResponse = JsonConvert.DeserializeObject<TestPagedList<ArticleResponse>>(responseString);
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             Assert.IsTrue(getResponse != null);
             Assert.IsTrue(getResponse.TotalCount > 0);
@@ -180,11 +179,15 @@ namespace DotNetCore.ApiIntegrationTests.Controllers
         {
             getHttpResponseMessage = Client.GetAsync($"api/v1/articles/{articleId}").Result;
             getResponseString = getHttpResponseMessage.Content.ReadAsStringAsync().Result;
-            getArticleResponse = JsonConvert.DeserializeObject<ArticleResponse>(getResponseString);
+
+            if (getHttpResponseMessage.StatusCode != HttpStatusCode.NotFound)
+                getArticleResponse = JsonConvert.DeserializeObject<ArticleResponse>(getResponseString);
+            else
+                getArticleResponse = null;
         }
 
         [TestMethod]
-        public async Task Put_Article_Should_Update_Article()
+        public void Put_Article_Should_Update_Article()
         {
             var createArticleRequest = new CreateArticleRequest()
             {
@@ -215,80 +218,147 @@ namespace DotNetCore.ApiIntegrationTests.Controllers
                 Title = GetUniqueStringValue(createArticleRequest.Title)
             };
 
-            UpdateArticle(createArticleResponse.Id, updateArticleRequest,out HttpResponseMessage updateHttpResponseMessage,out string updateResponseString);
+            UpdateArticle(createArticleResponse.Id, updateArticleRequest, out HttpResponseMessage updateHttpResponseMessage, out string updateResponseString);
 
             Assert.AreEqual(HttpStatusCode.NoContent, updateHttpResponseMessage.StatusCode);
 
+            GetArticle(createArticleResponse.Id,
+            out HttpResponseMessage getHttpResponseMessage,
+            out string getResponseString,
+            out ArticleResponse getArticleResponse);
 
-            var response3 = await Client.GetAsync($"api/v1/articles/{createArticleResponse.Id}");
-            response3.EnsureSuccessStatusCode();
-            var response3String = await response3.Content.ReadAsStringAsync();
-            var articleResponse3 = JsonConvert.DeserializeObject<ArticleResponse>(response3String);
+            getHttpResponseMessage.EnsureSuccessStatusCode();
 
-            Assert.AreEqual(HttpStatusCode.OK, response3.StatusCode);
-            Assert.AreEqual(updateArticleRequest.Title, articleResponse3.Title);
-            Assert.AreEqual(updateArticleRequest.Body, articleResponse3.Body);
-            Assert.AreEqual(updateArticleRequest.Author, articleResponse3.Author);
-            Assert.AreEqual(updateArticleRequest.IsPublished, articleResponse3.IsPublished);
-            Assert.AreEqual(updateArticleRequest.PublishDate, articleResponse3.PublishDate);
+            Assert.AreEqual(HttpStatusCode.OK, getHttpResponseMessage.StatusCode);
+            Assert.AreEqual(updateArticleRequest.Title, getArticleResponse.Title);
+            Assert.AreEqual(updateArticleRequest.Body, getArticleResponse.Body);
+            Assert.AreEqual(updateArticleRequest.Author, getArticleResponse.Author);
+            Assert.AreEqual(updateArticleRequest.IsPublished, getArticleResponse.IsPublished);
+            Assert.AreEqual(updateArticleRequest.PublishDate, getArticleResponse.PublishDate);
         }
 
-        //[TestMethod]
-        //public async Task Put_Article_Should_Return_BadRequest_Response_When_Creating_Article_With_Same_Title()
-        //{
-        //    var articleRequest = new CreateArticleRequest()
-        //    {
-        //        Title = GetUniqueStringValue("title_"),
-        //        Body = GetUniqueStringValue("body_"),
-        //        IsPublished = false,
-        //        PublishDate = null,
-        //        Author = "God",
-        //    };
+        [TestMethod]
+        public void Put_Article_Should_Return_BadRequest_Response_When_Creating_Article_With_Same_Title()
+        {
+            ArticleResponse CreateArticleRequestAndAssert()
+            {
+                var createArticleRequest = new CreateArticleRequest()
+                {
+                    Title = GetUniqueStringValue("title_"),
+                    Body = GetUniqueStringValue("body_"),
+                    IsPublished = false,
+                    PublishDate = null,
+                    Author = "God",
+                };
 
-        //    var response = await Client.PostAsJsonAsync("api/v1/articles", articleRequest);
+                CreateArticle(createArticleRequest,
+                out HttpResponseMessage createHttpResponseMessage,
+                out string createResponseString,
+                out ArticleResponse createArticleResponse);
 
-        //    var responseString = await response.Content.ReadAsStringAsync();
+                Assert.AreEqual(HttpStatusCode.Created, createHttpResponseMessage.StatusCode);
+                Assert.AreEqual(createArticleRequest.Title, createArticleResponse.Title);
+                Assert.AreEqual(createArticleRequest.Body, createArticleResponse.Body);
+                Assert.AreEqual(createArticleRequest.Author, createArticleResponse.Author);
+                Assert.IsTrue(createArticleResponse.Id > 1);
 
-        //    var articleResponse = JsonConvert.DeserializeObject<ArticleResponse>(responseString);
+                return createArticleResponse;
+            }
 
-        //    Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
-        //    Assert.AreEqual(articleRequest.Title, articleResponse.Title);
-        //    Assert.AreEqual(articleRequest.Body, articleResponse.Body);
-        //    Assert.AreEqual(articleRequest.Author, articleResponse.Author);
-        //    Assert.IsTrue(articleResponse.Id > 1);
+            var createArticleResponse1 = CreateArticleRequestAndAssert();
+            var createArticleResponse2 = CreateArticleRequestAndAssert();
 
+            var updateArticleRequest = new UpdateArticleRequest()
+            {
+                Title = createArticleResponse1.Title,
+                Body = GetUniqueStringValue("body_"),
+                IsPublished = true,
+                PublishDate = DateTime.UtcNow,
+                Author = "God",
+            };
 
-        //    var response2 = await Client.PostAsJsonAsync("api/v1/articles", articleRequest);
-        //    var response2String = await response2.Content.ReadAsStringAsync();
-        //    Assert.AreEqual(HttpStatusCode.BadRequest, response2.StatusCode);
-        //}
+            UpdateArticle(createArticleResponse2.Id, updateArticleRequest, out HttpResponseMessage updateHttpResponseMessage, out string updateResponseString);
+            Assert.AreEqual(HttpStatusCode.BadRequest, updateHttpResponseMessage.StatusCode);
+        }
 
-        //[TestMethod]
-        //public async Task Post_Article_Should_Return_BadRequest_Response_When_Creating_Article_Invalid_Data()
-        //{
-        //    var articleRequest = new CreateArticleRequest()
-        //    {
-        //        Title = GetUniqueStringValue("title_"),
-        //        Body = GetUniqueStringValue("body_"),
-        //        IsPublished = false,
-        //        PublishDate = null,
-        //        Author = "God",
-        //    };
+        [TestMethod]
+        public void Put_Article_Should_Return_BadRequest_Response_When_Creating_Article_With_Invalid_Data()
+        {
+            ArticleResponse CreateArticleRequestAndAssert()
+            {
+                var createArticleRequest = new CreateArticleRequest()
+                {
+                    Title = GetUniqueStringValue("title_"),
+                    Body = GetUniqueStringValue("body_"),
+                    IsPublished = false,
+                    PublishDate = null,
+                    Author = "God",
+                };
 
-        //    var response = await Client.PostAsJsonAsync("api/v1/articles", articleRequest);
-        //    var responseString = await response.Content.ReadAsStringAsync();
-        //    Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
-        //}
+                CreateArticle(createArticleRequest,
+                out HttpResponseMessage createHttpResponseMessage,
+                out string createResponseString,
+                out ArticleResponse createArticleResponse);
 
-        //[TestMethod]
-        //public async Task Post_Article_Should_Return_BadRequest_Response_When_Creating_Article_With_No_Data()
-        //{
-        //    var articleRequest = new CreateArticleRequest();
+                Assert.AreEqual(HttpStatusCode.Created, createHttpResponseMessage.StatusCode);
+                Assert.AreEqual(createArticleRequest.Title, createArticleResponse.Title);
+                Assert.AreEqual(createArticleRequest.Body, createArticleResponse.Body);
+                Assert.AreEqual(createArticleRequest.Author, createArticleResponse.Author);
+                Assert.IsTrue(createArticleResponse.Id > 1);
 
-        //    var response = await Client.PostAsJsonAsync("api/v1/articles", articleRequest);
-        //    var responseString = await response.Content.ReadAsStringAsync();
-        //    Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
-        //}
+                return createArticleResponse;
+            }
+
+            var createArticleResponse1 = CreateArticleRequestAndAssert();
+            var createArticleResponse2 = CreateArticleRequestAndAssert();
+
+            var updateArticleRequest = new UpdateArticleRequest()
+            {
+                Title = null,
+                Body = GetUniqueStringValue("body_"),
+                IsPublished = true,
+                PublishDate = DateTime.UtcNow,
+                Author = "God",
+            };
+
+            UpdateArticle(createArticleResponse2.Id, updateArticleRequest, out HttpResponseMessage updateHttpResponseMessage, out string updateResponseString);
+            Assert.AreEqual(HttpStatusCode.BadRequest, updateHttpResponseMessage.StatusCode);
+        }
+
+        #endregion
+
+        #region Delete
+
+        [TestMethod]
+        public async Task Delete_Article_Should_Delete()
+        {
+            var createArticleRequest = new CreateArticleRequest()
+            {
+                Title = GetUniqueStringValue("title_"),
+                Body = GetUniqueStringValue("body_"),
+                IsPublished = false,
+                PublishDate = null,
+                Author = "God",
+            };
+
+            CreateArticle(createArticleRequest,
+            out HttpResponseMessage createHttpResponseMessage,
+            out string createResponseString,
+            out ArticleResponse createArticleResponse);
+
+            Assert.AreEqual(HttpStatusCode.Created, createHttpResponseMessage.StatusCode);
+            Assert.AreEqual(createArticleRequest.Title, createArticleResponse.Title);
+            Assert.AreEqual(createArticleRequest.Body, createArticleResponse.Body);
+            Assert.AreEqual(createArticleRequest.Author, createArticleResponse.Author);
+            Assert.IsTrue(createArticleResponse.Id > 1);
+
+            var response = await Client.DeleteAsync($"api/v1/articles/{createArticleResponse.Id}");
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+
+            GetArticle(createArticleResponse.Id, out HttpResponseMessage getHttpResponseMessage, out string getResponseString, out ArticleResponse getArticleResponse);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, getHttpResponseMessage.StatusCode);
+        }
 
         #endregion
     }
